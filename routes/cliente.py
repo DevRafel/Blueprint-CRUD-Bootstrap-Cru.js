f681fc4268291d210c019aaa6172d3c607ed88e3
Rafael Lima <rafaelmoreiralimap4@gmail.com>
2024-02-10 15:29:52 -0300

    criando o esitar criente

--- routes/cliente.py
+++ routes/cliente.py
@@ -44,13 +44,31 @@ def detalhe_cliente(cliente_id):
 @cliente_route.route('/<int:cliente_id>/edit')
 def form_edit_cliente(cliente_id):
        """ editar um cliente """
-       return render_template('form_edit_cliente.html')
+       
+       cliente = None
+       for  c in CLIENTES:
+              if c ['id'] == cliente_id:
+                     cliente = c
+       
+       return render_template('form_cliente.html', cliente=cliente)
 
 
 @cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
 def atualizar_cliente(cliente_id):
        """ atualizar dados de 1 cliente """
-       pass
+       cliente_editado = None
+       # obter dados formulario edicao
+       data = request.json 
+       # obter usuario pelo id
+       for c in CLIENTES:
+              if c ['id'] == cliente_id:
+                     c['nome'] = data['nome']
+                     c['email'] = data['email']
+                     
+                     cliente_editado = c
+       # editar usuario
+       
+       return render_template('item_cliente.html', cliente=cliente_editado)
 
 
 @cliente_route.route('/<int:cliente_id>/delete', methods=['DELETE'])
